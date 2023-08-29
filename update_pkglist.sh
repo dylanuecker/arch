b09c164a556e93d2f2752b63bb562770be3036ff
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-08-29 12:10:23 -0400

    Update path

---
+++ update_pkglist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	filename=laptop-pkglist
+else
+	filename=pc-pkglist
+fi
+
+cd /home/dylan/.maintenance
+pacman -Qqe > $filename
+if [[ 'git status --porcelain $filename' ]]; then
+	git add $filename
+	git commit -m "Update package list"
+	git push
+fi
