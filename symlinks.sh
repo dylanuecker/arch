bd85d944bb799cd2e8d7fc99d8ab14061370e2b1
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-08-31 10:47:25 -0400

    Take action not reminders

---
+++ symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sr pkglist/package_list.hook /etc/pacman.d/hooks/package_list.hook
+
+ln -sr issue/update-issue.service /etc/systemd/system/update-issue.service
+systemctl enable update-issue.service
+
+ln -sr fonts ~/.local/share/fonts
