10a225f93c2303a550a2603ae0d203065107bba8
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-11 12:45:08 -0400

    No more wallpaper directory

--- symlinks.sh
+++ symlinks.sh
@@ -6,5 +6,3 @@ ln -sr issue/update-issue.service /etc/systemd/system/update-issue.service
 systemctl enable update-issue.service
 
 ln -sr fonts ~/.local/share/fonts
-
-ln -sr wallpaper ~/.config/wallpaper
