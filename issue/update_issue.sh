973a92dd94395c27e5324c647596cc8940b83ec0
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-11-14 18:18:57 -0500

    Change issue continuous message

--- issue/update_issue.sh
+++ issue/update_issue.sh
@@ -23,6 +23,6 @@ echo -n \
 
 $(printf "%0.s-" $(seq $max_line_len))
 
-\e{lightblue}got job?\e{reset}
+\e{lightblue}this too shall pass\nwon't be the last\e{reset}
 
 " > /etc/issue.d/fortune.issue
