b58ecb691a6c94de7c05e8270cfede8037961efc
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-11-24 14:15:22 -0500

    Fix glob path issue

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dir="~/arch"
+dir="/home/$(whoami)/arch"
 cd dir
 
 sudo ln -s $dir/pacman/pacman.conf /etc/pacman.conf
@@ -12,11 +12,14 @@ systemctl --user link pacman/archdots.service
 systemctl --user link pacman/archdots.timer
 systemctl --user enable archdots.timer
 
+mkdir -p ~/.local/share/fonts
 ln -s $dir/fonts/* ~/.local/share/fonts/
 sudo pacman -S ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono
 
+mkdir -p ~/.local/share/backgrounds
 ln -s $dir/backgrounds/* ~/.local/share/backgrounds/
 
 if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	mkdir -p ~/.local/bin
 	ln -s $dir/bin/laptop/* ~/.local/bin/
 fi
