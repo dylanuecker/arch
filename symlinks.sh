43d49a0a1b1e3615812ad344880764691fb87093
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-11-24 14:02:17 -0500

    Update symlink paths

--- symlinks.sh
+++ symlinks.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-sudo ln -sr pacman/pacman.conf /etc/pacman.conf
+dir="~/arch"
+cd dir
 
-sudo ln -sr issue/update-issue.service /etc/systemd/system/update-issue.service
+sudo ln -s $dir/pacman/pacman.conf /etc/pacman.conf
+
+sudo ln -s $dir/issue/update-issue.service /etc/systemd/system/update-issue.service
 sudo systemctl enable update-issue.service
 
 systemctl --user link pacman/archdots.service
 systemctl --user link pacman/archdots.timer
 systemctl --user enable archdots.timer
 
-ln -sr fonts/* ~/.local/share/fonts/
+ln -s $dir/fonts/* ~/.local/share/fonts/
 sudo pacman -S ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono
 
-ln -sr backgrounds/* ~/.local/share/backgrounds/
+ln -s $dir/backgrounds/* ~/.local/share/backgrounds/
 
 if [[ "$HOSTNAME" == "cloudbreak" ]]; then
-	ln -sr bin/laptop/* ~/.local/bin/
+	ln -s $dir/bin/laptop/* ~/.local/bin/
 fi
