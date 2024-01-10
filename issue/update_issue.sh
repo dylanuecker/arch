e10d8dcfa0bc85224235e54db70f3adfd2cfeb75
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-01-10 00:25:05 -0500

    Update issue

--- issue/update_issue.sh
+++ issue/update_issue.sh
@@ -3,7 +3,8 @@
 # using a systemd unit to generate a new fortune message at boot
 # default /etc/issue gets displayed first
 
-mkdir -p /etc/issue.d # you'll thank yourself in the future
+# you'll thank yourself in the future
+mkdir -p /etc/issue.d
 
 fortune=$(fortune -a)
 
@@ -21,8 +22,21 @@ echo -n \
 
 \e{lightred}$fortune\e{reset}
 
+\e{lightgreen}This too shall pass.\e{reset}
+
 $(printf "%0.s-" $(seq $max_line_len))
-
-\e{lightblue}This too shall pass.\e{reset}
-
 " > /etc/issue.d/fortune.issue
+
+echo -ne \
+'
+       \e[1;36m.
+      \e[1;36m/#\
+     \e[1;36m/###\      \e[1;37m               #     \e[1;36m| *
+    \e[1;36m/p^###\     \e[1;37m a##e #%" a#"e 6##%  \e[1;36m| | |-^-. |   | \ /
+   \e[1;36m/##P^q##\    \e[1;37m.oOo# #   #    #  #  \e[1;36m| | |   | |   |  X
+  \e[1;36m/##(   )##\   \e[1;37m%OoO# #   %#e" #  #  \e[1;36m| | |   | ^._.| / \ \e[0;37mTM
+ \e[1;36m/###P   q#,^\
+\e[1;36m/P^         ^q\ \e[0;37mTM
+
+
+' >> /etc/issue.d/fortune.issue
